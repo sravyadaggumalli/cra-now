7892a6142b5904719bb0cd582d93c520eeb0b928
Sravya Daggumalli <sravyadaggumalli@gmail.com>
2019-05-15 00:59:46 -0400

    fix: remove sections

--- src/components/RouteListener.js
+++ src/components/RouteListener.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import { withRouter } from "react-router-dom";
 
+/*
 const pathEngIdMapping = [
   { path: "/about", engId: 1601333430 },
   { path: "/topics", engId: 1601333430 },
   { path: "/", engId: 1601333430 }
 ];
+*/
 
 const Listener = withRouter(
   class extends React.Component {
@@ -15,35 +17,39 @@ const Listener = withRouter(
       props.history.listen(location => {
         var url = new URL(location.pathname, window.location.href);
         console.log(url.href);
+        /*
         const engIdMapping = pathEngIdMapping.find(
           mapping => mapping.path === location.pathname
         );
-        if (engIdMapping) {
-          window.lpTag.newPage(url.href, {
-            section: [engIdMapping.engId],
-            sdes: [
-              {
-                type: "cart",
-                total: 11.7,
-                numItems: 6,
-                products: [
-                  {
-                    product: {
-                      name: "prod1",
-                      category: "category",
-                      sku: "sku",
-                      price: 7.8
-                    },
-                    quantity: 1
-                  }
-                ]
-              }
-            ],
-            taglets: {
-              rendererStub: { divIdsToKeep: { "lp-chat": true } }
+        */
+        // if (engIdMapping) {
+        window.lpTag.newPage(url.href, {
+          section: [],
+          sdes: [
+            {
+              type: "cart",
+              total: 11.7,
+              numItems: 6,
+              products: [
+                {
+                  product: {
+                    name: "prod1",
+                    category: "category",
+                    sku: "sku",
+                    price: 7.8
+                  },
+                  quantity: 1
+                }
+              ]
             }
-          });
-        }
+          ],
+          taglets: {
+            rendererStub: {
+              //divIdsToKeep: { "lp-chat": true }
+            }
+          }
+        });
+        // }
       });
     }
     render() {
